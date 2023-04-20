9af5ab6ba62cc02f755d00c7262a3f027ffa392e
Stephen Ostapenko <stepanos2002@gmail.com>
2023-04-20 03:42:24 +0300

    add jar task for gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "osss.tetris"
@@ -56,4 +57,27 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("osss.tetris.Main")
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "processResources"))
+        archiveFileName.set("tetris.jar")
+        destinationDirectory.set(File("."))
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest {
+            attributes(mapOf("Main-Class" to application.mainClass))
+        }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+                .map { if (it.isDirectory) it else zipTree(it) } + sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar)
+    }
 }
